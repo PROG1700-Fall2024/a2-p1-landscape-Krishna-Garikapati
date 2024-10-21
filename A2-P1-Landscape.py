f10dcee08c2783bb66bd083506c41d3b4da23d6d
krishna-garikapati <w0502117@campus.nscc.ca>
2024-10-21 01:05:33 -0300

    Ready for marking

--- A2-P1-Landscape.py
+++ A2-P1-Landscape.py
@@ -1,19 +1,44 @@
 #Program 1 – Landscape Calculator
 #A landscaping company needs a program that computes the price of landscaping for a new housing development. 
 
-#Student #:     
-#Student Name:  
+#Student #:   W0502117  
+#Student Name:  Krishna Priyanka Garikapati
+
 
 def main():
-    # YOUR CODE STARTS HERE, each line must be indented (one tab)
+        
+    #Input values
+    Housenumber=int(input("Enter House Number: "))
+    depth=float(input("Enter property depth (feet): "))
+    width=float(input("Enter property width (feet): "))
+    GrassType=input("Enter type of grass (fescue, bentgrass, campus): ")
+    Trees=int(input("Enter the number of trees: "))
 
-
-
-
-
-
-
-
-    # YOUR CODE ENDS HERE
-
+    # Calculation
+    surfaceArea=depth*width
+    TreeCharge=Trees*100
+    Baselabourcharge=1000
+    #Area<=5000 sq ft
+    if surfaceArea<=5000:
+                if GrassType.lower()=="fescue":
+                    GrassPrice=0.05*surfaceArea
+                elif GrassType.lower()=="bentgrass":
+                    GrassPrice=0.02*surfaceArea
+                elif GrassType.lower()=="campus":
+                    GrassPrice=0.01*surfaceArea
+                else:
+                    print("error,Please re-enter correct grass type")
+                TotalCost=Baselabourcharge+GrassPrice+TreeCharge
+    #Area>5000 sq ft
+    elif surfaceArea>5000:
+                if GrassType.lower()=="fescue":
+                    GrassPrice=0.05*surfaceArea
+                elif GrassType.lower()=="bentgrass":
+                    GrassPrice=0.02*surfaceArea
+                elif GrassType.lower()=="campus":
+                    GrassPrice=0.01*surfaceArea
+                else:
+                    print("error,Please re-enter correct grass type")
+                TotalCost=Baselabourcharge+GrassPrice+TreeCharge+500
+    print("Total cost for house ",Housenumber, "is: ${0:.2f}".format(TotalCost))
 main()
